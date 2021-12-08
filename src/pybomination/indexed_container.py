0fc67b96ef3fa80f5b9ed25cd83294e9da98b341
nivpgir <Nivpgir@gmail.com>
2021-12-09 01:51:01 +0200

    got some of the tests passing, most get operations work

--- src/pybomination/indexed_container.py
+++ src/pybomination/indexed_container.py
@@ -1,4 +1,4 @@
-
+from collections.abc import Mapping, Sequence
 class MappingWrapper:
     def __init__(self, mapping):
         self._inner = mapping
@@ -13,7 +13,7 @@ class MappingWrapper:
         return (v for v in self._inner.values())
 
     def locs(self):
-        return (k for k in self._inner.keys()))
+        return (k for k in self._inner.keys())
 
 class SequenceWrapper:
     def __init__(self, sequence):
@@ -31,33 +31,39 @@ class SequenceWrapper:
     def locs(self):
         return (i for i in range(len(self._inner)))
 
-class IndexedContainer:
+class IndexedContainer(object):
 
     def __init__(self, container):
-        self._inner = self._wrap(container)
+        self._inner = _wrap(container)
 
     def __getattribute__(self, attr_name):
-        if attr_name in self._inner.values():
-            return self._get_wrapped_inner_val(loc)
+        inner = object.__getattribute__(self, "_inner")
+        if attr_name in inner.locs():
+            item = inner[attr_name]
+            try:
+                return object.__class__(self)(item)
+            except ValueError as _ve:
+                return item
 
         raise AttributeError
+
 
     def __getitem__(self, loc):
         return self._get_inner_val(loc)
 
 
-    def _wrap(self, container):
-        if isinstance(container, Mapping):
-            return MappingWrapper(container)
+def _wrap(container):
+    if isinstance(container, Mapping):
+        return MappingWrapper(container)
+    elif isinstance(container, Sequence):
+        return SequenceWrapper(container)
 
-        elif isinstance(container, Sequence):
-            return SequenceWrapper(container)
-
-        raise ValueError
-
-    def _get_wrapped_inner_val(loc):
-        return IndexedContainer(self._get_inner_val(loc))
+    raise ValueError
 
 
-    def _get_inner_val(loc):
-        return self._inner[loc]
+def _get_wrapped_inner_val(loc):
+    return IndexedContainer(self._get_inner_val(loc))
+
+
+def _get_inner_val(loc):
+    return self._inner[loc]
